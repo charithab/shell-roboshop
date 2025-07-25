8e588cc07c4d4841140b6f72af9acf1afebff5f5
charitha <charitha.harinath@gmail.com>
2025-07-25 15:40:30 +0530

    Roboshop shell

--- frontend.sh
+++ frontend.sh
@@ -30,32 +30,32 @@ VALIDATE(){
     fi
 }
 
-dnf module disable nginx -y
+dnf module disable nginx -y  &>>$LOG_FILE
 VALIDATE $? "Disabling Default Nginx"
 
-dnf module enable nginx:1.24 -y
+dnf module enable nginx:1.24 -y  &>>$LOG_FILE
 VALIDATE $? "Enabling Nginx:1.24"
 
-dnf install nginx -y
+dnf install nginx -y  &>>$LOG_FILE
 VALIDATE $? "Installing Nginx:1.24"
 
-systemctl enable nginx 
-systemctl start nginx 
+systemctl enable nginx  &>>$LOG_FILE
+systemctl start nginx   &>>$LOG_FILE
 VALIDATE $? "Starting nginx service"
 
-rm -rf /usr/share/nginx/html/* 
+rm -rf /usr/share/nginx/html/*  &>>$LOG_FILE
 VALIDATE $? "Removing default HTML content"
 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip  &>>$LOG_FILE
 VALIDATE $? "Downloading frontend"
 
 cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip  &>>$LOG_FILE
 VALIDATE $? "Unzipping frontend"
 
 rm -rf /etc/nginx/nginx.conf
-cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf  &>>$LOG_FILE
 VALIDATE $? "creating nginx.conf"
 
-systemctl restart nginx 
+systemctl restart nginx  &>>$LOG_FILE
 VALIDATE $? "Restarting nginx"
