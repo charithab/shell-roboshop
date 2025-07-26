1e3c6138d2f32f03967005c059ce24899acd51c1
charitha <charitha.harinath@gmail.com>
2025-07-26 14:57:00 +0530

    Roboshop shell

--- cart.sh
+++ cart.sh
@@ -9,6 +9,7 @@ N="\e[0m"
 SCRIPT_NAME=$(echo $0 | cut -d "." f1)
 LOGS_FOLDER="/var/log/roboshop-logs"
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
